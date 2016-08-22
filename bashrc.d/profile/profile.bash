2beafab50efb97f3979b953fc4663850c0726e5f
lethosor <lethosor@gmail.com>
2016-08-22 17:46:43 -0400

    cowsay: More variety

--- bashrc.d/profile/profile.bash
+++ bashrc.d/profile/profile.bash
@@ -1,7 +1,30 @@
-echo profile
+good_cows=(
+    "blowfish"
+    "cheese"
+    "cower"
+    "default"
+    "dragon-and-cow"
+    "dragon"
+    "elephant"
+    "flaming-sheep"
+    "ghostbusters"
+    "meow"
+    "milk"
+    "moofasa"
+    "moose"
+    "stegosaurus"
+    "turkey"
+    "turtle"
+    "tux"
+    "vader"
+)
 if [ -z "$bash_profile_loaded" ]
 then
     uptime
-    fortune | cowsay
+    cow=${good_cows[$RANDOM % (${#good_cows[@]}) ]}
+    if [[ -z "$cow" ]]; then
+        cow=default
+    fi
+    fortune | cowsay -f "$cow"
     export bash_profile_loaded=1
 fi

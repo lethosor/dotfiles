ad2d4e734cd75666ef7e45d7bfdfb31d79b19a32
lethosor <lethosor@gmail.com>
2016-08-22 15:56:55 -0400

    Install *.d folders

--- install.py
+++ install.py
@@ -21,18 +21,36 @@ with open(join(cur_path, 'dotfile_blacklist.txt')) as f:
     blacklist = f.read().replace('\r', '').split('\n')
     blacklist = list(filter(bool, blacklist))
 
+def check_existing_path(full_path, dest_path):
+    if realpath(dest_path) != realpath(full_path):
+        if dest_path == realpath(dest_path):
+            print('Error: Not linking %s: already exists' % dotfile)
+        else:
+            print('Error: Not linking %s: already linked to %s' % (dotfile, realpath(dest_path)))
+
+def link_path(full_path, dest_path):
+    print('Linking %s' % dotfile)
+    symlink(full_path, dest_path)
+
+
 for dotfile in listdir(cur_path):
-    if dotfile.startswith('.') and isfile(dotfile) and dotfile not in blacklist:
-        full_path = join(cur_path, dotfile)
-        dest_path = join(home_path, dotfile)
+    if dotfile in blacklist:
+        continue
+
+    full_path = join(cur_path, dotfile)
+    dest_path = join(home_path, dotfile)
+    if dotfile.startswith('.') and isfile(dotfile):
         if isfile(dest_path):
-            if realpath(dest_path) != realpath(full_path):
-                if dest_path == realpath(dest_path):
-                    print('Error: Not linking %s: already exists' % dotfile)
-                else:
-                    print('Error: Not linking %s: already linked to %s' % (dotfile, realpath(dest_path)))
+            check_existing_path(full_path, dest_path)
         elif exists(dest_path):
             print('Error: Not linking %s: already exists and is not a file' % dotfile)
         else:
-            print('Linking %s' % dotfile)
-            symlink(full_path, dest_path)
+            link_path(full_path, dest_path)
+
+    elif dotfile.endswith('.d') and isdir(dotfile):
+        if isdir(dest_path):
+            check_existing_path(full_path, dest_path)
+        elif exists(dest_path):
+            print('Error: Not linking %s: already exists and is not a directory' % dotfile)
+        else:
+            link_path(full_path, dest_path)

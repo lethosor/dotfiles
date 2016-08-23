1d757f408fc4743701d82aab76544eff71ec0fc5
lethosor <lethosor@gmail.com>
2016-08-23 12:10:13 -0400

    Improve cd/pushd behavior

--- bashrc.d/wrappers.bash
+++ bashrc.d/wrappers.bash
@@ -2,10 +2,14 @@
 
 alias mv='mv -i'
 alias cp='cp -i'
-alias cd='pushd >/dev/null'
 alias fail='tail -f'
 
 alias ssh="~/dotfiles/color-ssh/color-ssh.py"
+
+function cd {
+    pushd -n "$(pwd)" >/dev/null
+    command cd "$@"
+}
 
 function mcd {
     mkdir -p "$1"

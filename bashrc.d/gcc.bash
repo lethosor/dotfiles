e7a80c238cc0b5241b468e6946ecbb622d8c09a5
lethosor <lethosor@gmail.com>
2016-09-09 11:49:45 -0400

    Support environments with just gcc/g++

--- bashrc.d/gcc.bash
+++ bashrc.d/gcc.bash
@@ -1,4 +1,4 @@
-export CC="$(which gcc-4.8)"
-export CXX="$(which g++-4.8)"
+export CC="$(which gcc-4.8 2>/dev/null || which gcc)"
+export CXX="$(which g++-4.8 2>/dev/null || which g++)"
 export REAL_CC="$CC"
 export REAL_CXX="$CXX"

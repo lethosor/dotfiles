87805dc35fa6db38046682350c7bdeb04dca6cb2
lethosor <lethosor@gmail.com>
2018-06-07 16:09:48 -0400

    Change GCC to 7

--- bashrc.d/gcc.bash
+++ bashrc.d/gcc.bash
@@ -1,4 +1,4 @@
-export CC="$(which gcc-4.8 2>/dev/null || which gcc)"
-export CXX="$(which g++-4.8 2>/dev/null || which g++)"
+export CC="$(which gcc-7 2>/dev/null || which gcc)"
+export CXX="$(which g++-7 2>/dev/null || which g++)"
 export REAL_CC="$CC"
 export REAL_CXX="$CXX"

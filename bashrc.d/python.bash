86196816ca57650575d554f21dee02ab159cce67
lethosor <lethosor@gmail.com>
2019-05-31 21:15:16 -0400

    Add pyenv support

--- bashrc.d/python.bash
+++ bashrc.d/python.bash
@@ -1,1 +1,12 @@
 export PYTHONSTARTUP=~/.pythonstartup.py
+
+# do this before setting up pyenv
+if [ -z "$VIRTUALENVWRAPPER_PYTHON" ]; then
+    export VIRTUALENVWRAPPER_PYTHON="$(which python)"
+fi
+
+export PYENV_ROOT="$HOME/.pyenv"
+add_path PATH begin "$PYENV_ROOT/bin"
+if cmd_exists pyenv; then
+    eval "$(pyenv init -)"
+fi

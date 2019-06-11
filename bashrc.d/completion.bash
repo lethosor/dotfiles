3a16f99fc86418c0965c479c0e2688c2016ab072
lethosor <lethosor@gmail.com>
2019-06-11 13:19:20 -0400

    Add completion for git ancestor/vpush

--- bashrc.d/completion.bash
+++ bashrc.d/completion.bash
@@ -1,1 +1,10 @@
 [ -n "$HOMEBREW_PREFIX" ] && src_if_exists "$HOMEBREW_PREFIX/etc/bash_completion"
+
+_git_ancestor() {
+    __git_complete_refs $track_opt
+}
+
+_git_vpush() {
+    words[1]=push
+    _git_push "$@"
+}

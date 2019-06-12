d3498e2a78dd0f949c946c479624756ef45ad173
lethosor <lethosor@gmail.com>
2019-06-12 15:06:47 -0400

    git fetch-pr completion

--- bashrc.d/completion.bash
+++ bashrc.d/completion.bash
@@ -4,6 +4,10 @@ _git_ancestor() {
     __git_complete_refs $track_opt
 }
 
+_git_fetch_pr() {
+    test -z "${words[2]}" && __gitcomp_nl "$(__git_remotes)"
+}
+
 _git_vpush() {
     words[1]=push
     _git_push "$@"

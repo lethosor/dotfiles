5ec4816704cffee448c5fe9f79e9ed25136c4761
lethosor <lethosor@gmail.com>
2019-07-11 12:00:06 -0400

    perl.bash: use add_path

--- bashrc.d/perl.bash
+++ bashrc.d/perl.bash
@@ -1,5 +1,5 @@
-export PERL_LOCAL_LIB_ROOT="$HOME/perl5:$PERL_LOCAL_LIB_ROOT"
 export PERL_MB_OPT="--install_base \"$HOME/perl5\""
 export PERL_MM_OPT="INSTALL_BASE=$HOME/perl5"
-export PERL5LIB="$HOME/perl5/lib/perl5:$PERL5LIB"
+add_path PERL_LOCAL_LIB_ROOT begin "$HOME/perl5"
+add_path PERL5LIB begin "$HOME/perl5/lib/perl5"
 add_path PATH end ~/perl5/bin

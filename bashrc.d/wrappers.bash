3d7faf9404df401d7fff2f9127713c66ac1061c1
lethosor <lethosor@gmail.com>
2019-07-11 12:00:19 -0400

    reload: source instead of exec

--- bashrc.d/wrappers.bash
+++ bashrc.d/wrappers.bash
@@ -15,6 +15,13 @@ function cd {
     command cd "$@"
 }
 
+function reload {
+    if [ -n "$bash_profile_loaded" ]; then
+        . ~/.bash_profile
+    fi
+    . ~/.bashrc
+}
+
 function mcd {
     mkdir -p "$1"
     cd "$1"
@@ -23,13 +30,6 @@ function mcd {
 function unsafe-ssh {
     ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$@"
 }
-
-# maintain "-bash" vs "bash"
-if [[ "$(ps -p $$ -o command | tail -n1)" =~ ^- ]]; then
-    alias reload='test -n "$VIRTUAL_ENV" && deactivate; exec -l bash'
-else
-    alias reload='test -n "$VIRTUAL_ENV" && deactivate; exec bash'
-fi
 
 function txa {
     tmux attach -t "$1" 2>/dev/null || tmux new -s "$1"

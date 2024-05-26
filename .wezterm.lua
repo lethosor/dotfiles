1b1b9f9fb54229a6c373babf45c8cd143409063c
lethosor <lethosor@gmail.com>
2024-05-25 23:15:19 -0400

    Tab colors, fonts

--- .wezterm.lua
+++ .wezterm.lua
@@ -14,6 +14,10 @@ config.window_padding = {
   bottom = 1,
 }
 
+config.window_frame = {
+  font = wezterm.font{family='Ubuntu', weight='Bold'},
+}
+
 -- Cursor
 config.default_cursor_style = 'BlinkingBar'
 config.cursor_blink_ease_in = 'Constant'
@@ -25,6 +29,8 @@ config.cursor_blink_ease_out = 'Constant'
 -- config.color_scheme = 'Github Light (Gogh)'
 -- config.color_scheme = 'Google (light) (terminal.sexy)'
 -- config.color_scheme = 'Atelier Forest Light (base16)'
+local TAB_BG_INACTIVE = '#DCD8D4'
+local TAB_FG = '#333'
 config.colors = {
   foreground = '#2e3436',
   background = '#ffffff',
@@ -51,8 +57,29 @@ config.colors = {
     'rgb(63,205,205)',
     'rgb(255,255,255)',
   },
+
+  tab_bar = {
+    active_tab = {
+      bg_color = '#F6F5F4',
+      fg_color = TAB_FG,
+    },
+    inactive_tab = {
+      bg_color = TAB_BG_INACTIVE,
+      fg_color = TAB_FG,
+      strikethrough=true,
+    },
+    inactive_tab_hover = {
+      bg_color = '#E6E3E0',
+      fg_color = TAB_FG,
+    },
+  },
 }
+config.colors.tab_bar.new_tab = config.colors.tab_bar.inactive_tab
+config.colors.tab_bar.new_tab_hover = config.colors.tab_bar.inactive_tab_hover
 config.bold_brightens_ansi_colors = false
+
+config.window_frame.inactive_titlebar_bg = TAB_BG_INACTIVE
+config.window_frame.active_titlebar_bg = TAB_BG_INACTIVE
 
 -- Font
 config.font = wezterm.font('DejaVu Sans Mono')

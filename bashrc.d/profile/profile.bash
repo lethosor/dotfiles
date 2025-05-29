7e3c6eb4be568409972ac5d3392e581c704a0d90
lethosor <lethosor@gmail.com>
2025-05-29 01:19:52 -0400

    silence startup output on non-ttys

--- bashrc.d/profile/profile.bash
+++ bashrc.d/profile/profile.bash
@@ -3,9 +3,11 @@
 
 if [ -z "$bash_profile_loaded" ]
 then
-    uptime
-    if cmd_exists fortune && cmd_exists cowsay; then
-        . ~/bashrc.d/profile/cows.bash
+    if [ -t 0 ] && [ -t 1 ] && [ -t 2 ]; then
+        uptime
+        if cmd_exists fortune && cmd_exists cowsay; then
+            . ~/bashrc.d/profile/cows.bash
+        fi
     fi
     export bash_profile_loaded=1
 fi
